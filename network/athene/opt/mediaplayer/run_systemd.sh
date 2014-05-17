0b90b505a7c25a1c8d0fc5b68cbe19b9c1599aed
Markus M. May <triplem@javafreedom.org>
2014-05-17 21:05:37 +0200

    Add athene

---
+++ network/athene/opt/mediaplayer/run_systemd.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# Absolute path to this script, e.g. /home/pi/mediaplayer/run.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/pi/mediaplayer
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd "${SCRIPTPATH}"
+echo ${SCRIPTPATH}
+
+export DISPLAY=:0
+xset s off
+xset -dpms
+
+java -jar "${SCRIPTPATH}"/mediaplayer.jar > /dev/null &
+
